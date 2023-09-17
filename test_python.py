5116ef5d04fce64ed69038afd67f5959a5820ba8
AnantUpadhyay02 <amanupadhyay1719@gmail.com>
2023-09-17 20:29:48 +0530

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,3 +1,20 @@
+#Printing something
 a= 3
 print(a)
+
+#Lists: making and operstaions
+
+food = ["Fries","Onion_rings", "chips"]
+drinks = ["Rum", "whiskey", "Vodka"]
+
+bar = food + drinks
+
+print(bar)
+
+drinks.append("Cocktails")
+food.insert(2,"Cheese_cubes")
+
+print(food,bar)
+
+
 
